658574ce9d956264d9b8b7694faf8e21bbfdd731
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-13 22:23:18 +0200

    Fetch now fully workign. Exception handling

--- DBFetch.py
+++ DBFetch.py
@@ -3,13 +3,15 @@ import asyncio
 import discord
 from discord.ext import commands
 import json
+import datetime
 
-bot = commands.Bot(command_prefix='-', description='Fetches all messages in a server and adds it to a SQLite DB.')
+bot = discord.Client()
 
 with open('Vars.json', 'r') as v:
     vars = json.load(v)
 
 db = sqlite3.connect('messages.sqlite')
+c = db.cursor()
 print(f'Connected to database.')
 
 @bot.event
@@ -19,13 +21,29 @@ async def on_ready():
 
 async def fetch():
     g = bot.get_guild(365442290992545792)
+    messages = 0
     for ch in g.channels:
         if isinstance(ch, discord.TextChannel):
-            messages = await ch.history().flatten()
-            for m in messages:
-                if m.author.bot == False:
-                    print(m)
+            print(f'Starting {ch.name} at {datetime.datetime.now().time()}')
+            try:
+                async for m in ch.history(limit=None):
+                    if m.author.bot == False:
+                        messages += 1
+                        if messages%50 == 0:
+                            print(f'Parsed {messages} messages.')
+                        if messages >= 1000:
+                            db.commit()
+                            messages = 0
+                            print(f'Commiting - Parsed 1000 messages in {ch.name}.')
+                        c.execute("INSERT INTO messages (message_id, user_id, user_name, content, guild_id, channel_id, channel_name) VALUES (?,?,?,?,?,?,?)",
+                        (m.id, m.author.id, m.author.display_name, m.content, m.guild.id, m.channel.id, m.channel.name))
+                print(f'Done with {ch.name} at {datetime.datetime.now().time()}')
+            except Exception as e:
+                print(f'Access forbidden for {ch.name} | {e}')
+    db.commit()
+    print('All done.')
 
+#message_id int, user_id int, user_name text, content text, guild_id int, channel_id int, channel_name text
 
 
 bot.run(vars['token'])
